344a9c4ccbb941c8b3b808ddf9fd60a60f5ca407
FZambia <frvzmb@gmail.com>
2015-02-09 00:01:49 +0300

    supporting function for generation private channel auth

--- cent/core.py
+++ cent/core.py
@@ -19,6 +19,7 @@ except ImportError:
 import six
 import hmac
 import json
+from hashlib import md5, sha256
 
 
 def generate_token(secret_key, project_id, user_id, timestamp, user_info=None):
@@ -26,6 +27,12 @@ def generate_token(secret_key, project_id, user_id, timestamp, user_info=None):
     When client from browser wants to connect to Centrifuge he must send his
     user ID and ID of project. To validate that data we use HMAC to build
     token.
+
+    TODO:
+    if user_info is None:
+        user_info = json.dumps({})
+    And get rid of if condition
+    And rename `user_info` into `info`
     """
     sign = hmac.new(six.b(str(secret_key)))
     sign.update(six.b(project_id))
@@ -35,6 +42,20 @@ def generate_token(secret_key, project_id, user_id, timestamp, user_info=None):
         sign.update(six.b(user_info))
     token = sign.hexdigest()
     return token
+
+
+def authenticate(secret_key, client_id, channel, info=None):
+    """
+    Generate auth HMAC for private channel subscription
+    """
+    if info is None:
+        info = json.dumps({})
+
+    auth = hmac.new(six.b(str(secret_key)), digestmod=sha256)
+    auth.update(six.b(str(client_id)))
+    auth.update(six.b(str(channel)))
+    auth.update(six.b(info))
+    return auth.hexdigest()
 
 
 class Client(object):
