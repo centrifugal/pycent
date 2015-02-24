26dd5d2aa97623a7eb33f5c3241da620dd23edce
FZambia <frvzmb@gmail.com>
2015-02-24 17:25:05 +0300

    v0.3.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def long_description():
 
 setup(
     name='cent',
-    version='0.3.0dev',
+    version='0.3.0',
     description="python tools to communicate with Centrifuge",
     long_description=long_description(),
     url='https://github.com/centrifugal/cent',
